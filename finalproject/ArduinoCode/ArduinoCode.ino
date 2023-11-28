5f2743450c1353a5cc64802df38237b7c6418f33
Saif <skazmi21@illinois.edu>
2023-11-27 19:59:55 -0600

    code added to close/open hand

--- finalproject/ArduinoCode/ArduinoCode.ino
+++ finalproject/ArduinoCode/ArduinoCode.ino
@@ -1,5 +1,28 @@
+// Elbow motor setup
+int enElbow = 5;
+int in1Elbow = 2;
+int in2Elbow = 3;
+// Hand motor setup
+int enHand = 6;
+int in3Hand = 7;
+int in4Hand = 8;
+int handPosition = 0;
+
 void setup() {
   Serial.begin(115200); // Start the serial communication at 115200 baud rate
+  	// Set all the motor control pins to outputs
+	pinMode(enElbow, OUTPUT);
+	pinMode(enHand, OUTPUT);
+	pinMode(in1Elbow, OUTPUT);
+	pinMode(in2Elbow, OUTPUT);
+	pinMode(in3Hand, OUTPUT);
+	pinMode(in4Hand, OUTPUT);
+	
+	// Turn off motors - Initial state
+	digitalWrite(in1Elbow, LOW);
+	digitalWrite(in2Elbow, LOW);
+	digitalWrite(in3Hand, LOW);
+	digitalWrite(in4Hand, LOW);
 }
 
 void loop() {
@@ -9,5 +32,21 @@ void loop() {
   // Send the sensor value and voltage over serial
   //Serial.print("Sensor Value: ");
   Serial.println(sensorValue);
+  int newHandPosition = analogRead(A2);
+  // printf(newHandPosition);
+  if(handPosition < newHandPosition){
+	digitalWrite(in3Hand, HIGH);
+	digitalWrite(in4Hand, LOW);
+	analogWrite(enHand, 255);
+	delay(850);
+	analogWrite(enHand, 0);
+  }else if(handPosition > newHandPosition){
+	digitalWrite(in3Hand, LOW);
+	digitalWrite(in4Hand, HIGH);
+	analogWrite(enHand, 255);
+	delay(850);
+	analogWrite(enHand, 0);
+  }
+  handPosition = newHandPosition;
   delay(10); // Delay for 10 milliseconds (adjust as needed)
 }
